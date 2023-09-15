93578bb88d201958ab1d6ed1943da71af510bdc2
Marat-Shainurov <m_shainurov@mail.ru>
2023-09-15 16:24:31 +0300

    adds basic CORS settings

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.endpoints.secrets import secrets
 
 from dotenv import load_dotenv
 
 load_dotenv()
+origins = ["http://localhost:8000"]
 
 app = FastAPI()
 app.include_router(secrets)
+app.add_middleware(CORSMiddleware, allow_origins=origins)
